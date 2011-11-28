b3682ad1fc0e8a45b62f1983d7e22025957fd389
Chase Higgins <chasehx@gmail.com>
2011-11-28 22:54:54 +0000

    backend coding

--- algo/dealrank.py
+++ algo/dealrank.py
@@ -1,6 +1,7 @@
 # DealRank Algorithm 0.1 Beta by Chase Higgins for deals website
 # TODO:
     # Multiprocessing. 
+    # Write it?
     
 # investigate the most efficient module for Python SQL, the libraries I have used were sub par at best
 # determine if PHP is faster than Python for this particular job, Python first since prototyping is faster
