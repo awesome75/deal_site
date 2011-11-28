7c500d517e1b706ba7c4de5d080cb2f00157c267
Chase Higgins <chasehx@gmail.com>
2011-11-28 23:45:41 +0000

    working on location scripts

---
+++ python/dealrank.py
@@ -0,0 +1,12 @@
+# DealRank Algorithm 0.1 Beta by Chase Higgins for deals website
+# TODO:
+    # Multiprocessing. 
+    # Write it?
+<<<<<<< HEAD
+    
+=======
+    # We will need to write a few different ranking algos, make sure this code is portable
+>>>>>>> 8470f104cd5a4becd40fc69dbe897be2027329d2
+# investigate the most efficient module for Python SQL, the libraries I have used were sub par at best
+# determine if PHP is faster than Python for this particular job, Python first since prototyping is faster
+# Jon you will be responsible for this algorithm pretty much. Have fun man.
