3d65c677f252b8240ec71168ac4827dcc19f383a
tzDev <chasehx@gmail.com>
2011-11-28 22:21:58 -0700

    changed user class

--- php/classes.php
+++ php/classes.php
@@ -57,7 +57,7 @@ class user {
     // this is the object that we will use to store user information in memory
     // declare the properties of the user
     var $user_id;
-    var $user_name;
+    var $name;
     var $password; // probably won't use this, all we have is our hashes, and why store a hash in memory?
     var $last_login;
     var $creation_date;
