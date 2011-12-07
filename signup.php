19897bff123d1b7f9af4281599e9845642c994cc
Chase Higgins <root@li251-163.members.linode.com>
2011-12-07 11:53:31 -0500

    membership system work

--- signup.php
+++ signup.php
@@ -4,6 +4,12 @@ require_once('php/classes.php'); // get our classes and functions
 // begin ip geo tracking
 $ipinf = getClientLocData();
 // end of location stuff
+if ($_POST['user_name'] && $_POST['password'] && $_POST['email']) {
+    // if the form is to be submitted
+    include('php/add_user.php');
+    // that should do it, now all we do is alter page flow to show user was added
+    $user_added = 1;
+}
 $ptitle = "Sign Up";
 include('html/header.html');
 ?>
@@ -16,7 +22,13 @@ include('html/header.html');
 <?
 include('html/deals_container.html');
 // signup content
-include('html/signup.html');
+if ($user_added == 1) {
+    echo "user adding was attempted";
+}
+
+else {
+    include('html/signup.html');
+}
 // finish page
 closeSidebar();
 include('html/footer.html');
