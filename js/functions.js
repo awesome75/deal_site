b7fe8f6b4e6441982cd0a9b7e46d3290f5bf8758
tzDev <chasehx@gmail.com>
2011-12-09 13:06:41 -0700

    adding functions

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,11 @@
 function clearInput(name) {
-  // clear a specified input box of it's defualt value when a user clicks the field
+	// clear a specified input box of it's defualt value when a user clicks the field
 	input = document.getElementsByName(name)[0];
 	input.value = ''; // blank out the input value
+}
+
+function setInputVal(name, val) {
+	// set the input box to display specified text as it's 'value'
+	input = document.getElementsByName(name)[0];
+	input.value = val;
 }
