a0fdd100b6520c599e0d999504cfcc94bc2b6b84
Chase Higgins <chasehx@gmail.com>
2011-12-21 19:02:27 -0500

    creating deal ajax

--- js/deal_suggest.js
+++ js/deal_suggest.js
@@ -1,9 +1,42 @@
 function dealSuggest() {
     // AJAX deal suggest
     // first we need to determine the type of deals we are going to bring back
-    var company, location, price, type, req, resp;
+    var company, location, price, type, req, resp, deal_objects, deals;
+    this.createDealHtml = function(deal_objects) {
+        // make the HTML code for the deals 
+        var deals = [];
+        for (var i = 0; i < deal_objects.length; i++) {
+            // iterate the deal objects and build some deals
+            // model the way it is built of deal.html
+            var container, title, title_link, info_container, byline;
+            // first the container
+            container = document.createElement('div');
+            container.setAttribute('class', 'deal');
+            // the title
+            title = document.createElement('h1');
+            title_link = document.createElement('a');
+            title_link.href = "/deal_site/?deal_id=" + deal_objects[i].deal_id;
+            title_link.innerHTML = deal_objects[i].deal_title;
+            title.appendChild(title_link);
+            // now we go to the deal_info div
+            info_container = document.createElement('div');
+            info_container.setAttribute('class', 'deal_info');
+            byline = document.createElement('span');
+            byline.setAttribute('class', 'byline');
+            byline.innerHTML = deal_objects[i].deal_poster_id + " on " + deal_objects[i].deal_post_date;
+            info_container.appendChild(byline);
+            
+            // append to the container
+            container.appendChild(title);
+            container.appendChild(info_container);
+            console.log(container);
+        }
+        // now return the deals as HTML objects
+        return deals;
+    };
+    req = getXmlHttp();
     try {
-        company = document.getElementsByName('company')[0].valie;
+        company = document.getElementsByName('company')[0].value;
     }
     catch(e) {
         company = undefined;   
@@ -29,11 +62,12 @@ function dealSuggest() {
     // now we go through each one and see what ones were defined
     if (company) {
         // we will do a company suggest for the deals  
-        req = getXmlHttp();
         req.onreadystatechange = function() {
             if (req.readyState == 4 && req.status == 200) {
                 // we're good to parse results   
                 resp = req.responseText.trim();
+                deal_objects = JSON.parse(resp);
+                deals = createDealHtml(deal_objects);
             }
         };
         req.open('GET', 'php/deal_suggest.php?company='+company+'&location=&price=&type=', true);
--- php/deal_suggest.php
+++ php/deal_suggest.php
@@ -2,13 +2,18 @@
 require_once('classes.php');
 
 class deal_suggest {
-    var $location = SQLClean($_GET['location']);   
-    var $company = SQLClean($_GET['company']);
-    var $price = SQLClean($_GET['price']);
-    var $type = SQLCLean($_GET['type']);
-
-    function determineQuery() {
+    var $location;
+    var $company;
+    var $price;
+    var $type;
+    var $deals;
+    
+    function getSuggestion() {
         if (!is_int($this->company)) {
+            $this->location = SQLClean($_GET['location']);   
+            $this->company = SQLClean($_GET['company']);
+            $this->price = SQLClean($_GET['price']);
+            $this->type = SQLCLean($_GET['type']);
             // we need to retrieve the company ID
             $sql = sprintf("SELECT `company_id` FROM `companies` WHERE `company_name` = '%s'", trim($this->company));
             $con = getSQLConnection('deal_site');
@@ -23,16 +28,36 @@ class deal_suggest {
             mysql_close($con);
         }
         // now we know we have a company id and can move on
-        if ($company != "" && $price == "" && $type == "") {
+        if ($this->company != "" && $this->price == "" && $this->type == "") {
             // we are going company only
-            $deals = getDeals(null, null, null, $this->company);
+            $this->deals = getDeals(null, null, null, $this->company);
+            if (is_array($this->deals)) {
+                // this means we made great success
+                return 1;
+            }
         }
+    } // end of getSuggest() method
+    
+    function encodeDeals() {        
+        return json_encode($this->deals);
+    } // end of returnDeals() method
+} // end of deal suggest class
+
+
+function main() {
+    $suggest = new deal_suggest();
+    $result = $suggest->getSuggestion();
+    if ($result != 1) {
+        die('fail');
     }
+    // if the script is still running we will need to parse and return data to the calling JS function
+    $response = $suggest->encodeDeals();
+    echo $response; // echo it out to the script
+    return 1; // success
+} // end of main
 
-
-
-
-
-
-}
+main();
 ?>
+
+
+
