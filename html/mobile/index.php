10db8b8bc05cf9c77a3c214278a23f38418209f5
Chase Higgins <chasehx@gmail.com>
2013-03-01 15:01:31 -0700

    start mobile area

---
+++ html/mobile/index.php
@@ -0,0 +1,62 @@
+<?
+  session_start();
+  //var_dump($_SESSION);
+  //echo "<br /><br />";
+?>
+<!doctype html>
+<html>
+<head>
+<title>Deals Mobile</title>
+<meta name="HandheldFriendly" content="True">
+<!--<meta name="MobileOptimized" content="320">-->
+<meta name="viewport" content="width=device-width">
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  
+  html, body {
+    font-family: Helvetica, Verdana, Arial, sans-serif;
+    font-size: 12px;
+    zoom: 1;
+  }
+  
+  #header {
+    background-color: #AAD874;  
+    -webkit-box-shadow: 0 0 5px 3px #888;
+    -moz-box-shadow: 0 0 5px 3px #888;
+    box-shadow: 0 0 5px 3px #888;
+    position: relative;
+    height: 70px;
+  }
+  
+  #header_right {
+    border: 1px solid black;
+    position: absolute;
+    right: 0;
+    display: inline-block;
+  }
+  
+  #header_user_tools {
+    background: #DDD;  
+  }
+</style>
+</head>
+<body>
+  <div id="header">
+    <div id="header_right">
+      hello world
+    </div>
+    k
+  </div><!-- end header -->
+  <div id="header_user_tools">
+    <br /><br />
+  </div><!-- end of header tools -->
+  <!-- logo div ends start page -->
+  <div id="main">        
+  
+  
+  </div><!-- end main -->
+</body>
+</html>
